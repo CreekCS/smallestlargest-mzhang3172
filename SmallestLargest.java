37e17fb6da484d73e24222715be0365df9f559d6
mzhang3172 <mzhang3@cherrycreekschools.org>
2024-08-21 19:54:29 +0000

    hi

--- SmallestLargest.java
+++ SmallestLargest.java
@@ -1,17 +1,28 @@
 //put your name heremichael zhang
-public class SmallestLargest{
-    public static void main(String[] args){
+import java.util.Random;
+
+public class SmallestLargest {
+    public static void main(String[] args) {
         int[] numbers = new int[10];
+        
         Random random = new Random();
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = random.nextInt(100) + 1;
         }
+
+
+        System.out.print("Array: ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
         System.out.println("Smallest value: " + smallestValue(numbers));
         System.out.println("Largest value: " + largestValue(numbers));
     }
 
-    public static int smallestValue(){
- int smallest = array[0];
+    public static int smallestValue(int[] array) {
+        int smallest = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] < smallest) {
                 smallest = array[i];
@@ -19,8 +30,9 @@ public class SmallestLargest{
         }
         return smallest;
     }
-    public static int largestValue(){
-         int largest = array[0];
+
+    public static int largestValue(int[] array) {
+        int largest = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] > largest) {
                 largest = array[i];
